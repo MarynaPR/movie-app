3574640fd17317f9fed6f788416f87948a4e618e
MarynaPr <pryadkamaryna@gmail.com>
2022-04-08 17:53:12 -0500

    added style to nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
   const router = useRouter()
 
   return (
-    <dev className= "flex justify-center bg-slate-700 text-rose-500 select-none text-xl lg:text-2xl">
+    <dev className= "flex justify-center font-bold bg-slate-700 text-rose-500 select-none text-xl lg:text-2xl">
       {Object.entries(requests).map(([key, { title, url }]) => (
 
       <h2 onClick ={ ()=>(router.push(`?genre=${key}`))} className='m-6 cursor-pointer hover:text-white active:text-yellow-200'
