1a439623c84c8739dfee5ff68f5b000ed97834d8
MarynaPr <pryadkamaryna@gmail.com>
2022-04-08 17:19:49 -0500

    card styling

--- components/Card.js
+++ components/Card.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
-import Results from './Results'
 import { ThumbUpIcon } from '@heroicons/react/solid'
 
 export default function Card({ result }) {
     const BASE_URL = "https://image.tmdb.org/t/p/original/"
     return (
-        <div>
-            <Image layout="responsive"
-                src={BASE_URL + result.backdrop_path}
-                width={200}
-                height={100}
-            />
-            <div className=''>
-                <p className='truncate'>{result.overview}</p>
-                <h2 className=''>{result.title || result.name}</h2>
-                <p>{result.release_date || result.first_air_date}</p>
-                <ThumbUpIcon className='h-5' /> {result.vote_count}
+        <div className="p-3 cursor-pointer xl:hover:scale-105 transition-transform duration-200">            <Image
+            layout="responsive"
+            src={BASE_URL + result.backdrop_path}
+            width={200}
+            height={100}
+        />
+            <div className='p-2'>
+                <h2 className='text-lg font-bold hover:text-blue-400 cursor-pointer active:text-rose-500'>{result.title || result.name}</h2>
+                <p className='truncate text-lg'>{result.overview}</p>
+                <p className='flex items-center '>{result.release_date || result.first_air_date}
+                    <ThumbUpIcon className='h-5 ml-3 mr-1' /> {result.vote_count}</p>
             </div>
-        </div>
+        </div >
     )
 }
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function HeaderIcon({Icon, title}) {
     return (
-        <div className='mx-4 felx flex-col cursor-pointer hover:text-rose-500 active:text-slate-400 lg:mx-6'>
+        <div className='mx-4 flex flex-col cursor-pointer hover:text-rose-500 active:text-slate-400 lg:mx-6'>
             <Icon className="h-8"/>
             <p className='my-2'>{title}</p>
         </div>
